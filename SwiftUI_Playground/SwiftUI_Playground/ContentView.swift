dd68429c18278a60fbd7eaf4e9ddf643550e18c3
Jordan Christensen <jordan.c4922@gmail.com>
2019-12-07 02:18:53 +0800

    Update gitignore, add initial project

---
+++ SwiftUI_Playground/SwiftUI_Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Playground
+//
+//  Created by Jordan Christensen on 12/7/19.
+//  Copyright © 2019 Mazjap Co. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
